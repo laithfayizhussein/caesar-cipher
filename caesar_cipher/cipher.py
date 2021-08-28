bbf714f3777233a1e3106fea7a9196e8868adae4
laithfayizhussein <laithalsanory9919@gmail.com>
2021-08-29 00:36:30 +0300

    finsih

--- caesar_cipher/cipher.py
+++ caesar_cipher/cipher.py
@@ -9,7 +9,7 @@ def encrypt(plain,key):
     new_char = char
    
     if new_char.isalpha(): #The isalpha() method returns True if all the characters are alphabet letters (a-z).
-     
+    
       if char.isupper():#The isupper() method returns True if all the characters are in upper case, otherwise False.
         
         new_char = chr((ord(char)+ key- 65) % 26 + 65) #The ord() function returns an integer representing the Unicode character.
@@ -19,3 +19,29 @@ def encrypt(plain,key):
     encrypted_string += new_char
 
   return encrypted_string
+ 
+def decrypt(ciper_text,key):
+  return encrypt(ciper_text,-key)
+
+
+def crack(text):
+  max=50
+  be_able = ''
+  
+  for shift in range(0,26):
+    encrypted_words =decrypt(text,shift)
+    split_word_list =encrypted_words.split() 
+  
+    count = 0 
+    
+    for word in split_word_list:
+      clean_word = re.sub(r"[^a-zA-Z]+", "", word).lower()
+    
+      if (clean_word in words_list) or (clean_word in names_list): 
+        count += 1
+    percent=int(count / len(split_word_list) * 100)
+    
+    if percent > max:
+      max=percent
+      be_able=encrypted_words
+  return be_able 
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,30 @@
-from caesar_cipher import __version__
+from caesar_cipher.cipher import crack, encrypt, decrypt
+import pytest
 
+def test_encrypt_my_name():
+    encrypted_word =encrypt('garfield',2)
+    assert encrypted_word == "icthkgnf"
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_decrypt():
+    encrypted_word = 'icthkgnf'
+    actual = decrypt(encrypted_word,2)
+    assert actual == 'garfield'
+
+def test_cipher_all_caps():
+    encrypted_word = encrypt('BASS',2)
+    actual = crack(encrypted_word)
+    expected = 'BASS'
+    assert actual == expected
+    
+def test_crack():
+    encrypted_word=encrypt("python", 1)
+    actual = crack(encrypted_word)
+    expected = "python"
+    assert actual == expected
+
+def test_crack_sentence():
+    sentence = 'i am a laith'
+    encrypted_word =encrypt(sentence, 1)
+    actual =crack(encrypted_word)
+    expected =sentence
+    assert actual == expected
